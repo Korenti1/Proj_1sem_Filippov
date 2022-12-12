ab0934c1d8cc96e59fe87d721d3c41eb577f9308
Олег Филиппов <90416289+Korenti1@users.noreply.github.com>
2022-12-12 11:13:21 +0300

    Update PZ_10.py

--- PZ_10.py
+++ PZ_10.py
@@ -9,5 +9,4 @@ thrd_sad = {'Кукла', 'Конструктор',}
 
 print(toys - fir_sad - sec_sad - thrd_sad)
 
-print(*(toys & fir_sad & sec_sad & thrd_sad),
-      'есть в каждом детском саду' if toys & fir_sad & sec_sad & thrd_sad != set() else 'Общих игрушек из списка нет ни в одном детском саду')
+print(*(toys & fir_sad & sec_sad & thrd_sad),'есть в каждом детском саду' if toys & fir_sad & sec_sad & thrd_sad != set() else 'Общих игрушек из списка нет ни в одном детском саду')
